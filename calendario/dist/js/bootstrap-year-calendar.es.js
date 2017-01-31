d70cc6d07e3a1e0366dedb2bdb3046e1d3780508
ravila <ravila@cronista.com>
2017-01-31 09:46:34 -0300

    commit meh

---
+++ calendario/dist/js/bootstrap-year-calendar.es.js
@@ -0,0 +1,4 @@
+/*
+  2017-01-26 
+*/
+!function(a){a.fn.calendar.dates.es={days:["Domingo","Lunes","Martes","Mi�rcoles","Jueves","Viernes","S�bado"],daysShort:["Dom","Lun","Mar","Mi�","Jue","Vie","S�b"],daysMin:["Do","Lu","Ma","Mi","Ju","Vi","Sa"],months:["Enero","Febrero","Marzo","Abril","Mayo","Junio","Julio","Agosto","Septiembre","Octubre","Noviembre","Diciembre"],monthsShort:["Ene","Feb","Mar","Abr","May","Jun","Jul","Ago","Sep","Oct","Nov","Dic"],weekShort:"S",weekStart:1}}(jQuery);
